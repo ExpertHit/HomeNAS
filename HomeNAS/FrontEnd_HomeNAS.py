84e02c41a0793adf4f1080f748d9ae924ab41ee7
ExpertHit <128145058+ExpertHit@users.noreply.github.com>
2023-07-19 13:35:21 +0200

    Update FrontEnd_HomeNAS.py
    Modification de la fonction du bouton "Se connecter à un serveur.."
    - Correction de l'emplacement du cadre qui apparait
    - Ajout d'une indication pour l'état de connexion du NAS
    - Ajout d'un une case à cocher qui permet de choisir le serveur connecté avec le bouton "Fichiers"
    - Ajout d'un bouton pour supprimer le cadre
    - Ajout de la possibilité d'ajouter plusieurs cadres pour les serveurs en entrant leurs IPs.

--- HomeNAS/FrontEnd_HomeNAS.py
+++ HomeNAS/FrontEnd_HomeNAS.py
@@ -8,13 +8,36 @@ import os
 import sys
 import requests
 import tkinter as tk
+import psutil
 print(tk.TkVersion)
 
+def close_port():
+    for proc in psutil.process_iter():
+        try:
+            for conn in proc.connections():
+                if conn.laddr.port == 8000:
+                    proc.kill()
+                    print("Port fermé avec succès")
+                    return
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    print("Erreur lors de la fermeture du port")
+
 ip_address = ""
+i = 0
 file_dir = os.path.dirname(__file__)
 
 def btn_clicked():
     print("Button Clicked")
+
+def shutdown_app():
+    # Fermer le processus utilisant le port 8000
+    subprocess.run(["lsof", "-ti", ":8000", "-sTCP:LISTEN", "-n", "-P", "-Fp"]) 
+
+def on_closing():
+    # Appelé lorsque la fenêtre est fermée
+    shutdown_app()
+    window.destroy()
 
 def start_nas_serv():
     python_executable = sys.executable
@@ -39,28 +62,90 @@ def browse_for_upload():
 
 def show_ip_entry():
     global ip_address
+    global i
     ip = simpledialog.askstring("Adresse IP", "Entrez l'adresse IP du serveur NAS :")
     if ip:
         ip_address = ip
-        create_ip_frame(ip)
+        create_ip_frame(ip,0, i)
         messagebox.showinfo("Adresse IP", f"L'adresse IP saisie est : {ip}")
+        i = i + 1
     else:
         messagebox.showwarning("Adresse IP", "Aucune adresse IP saisie")
 
 
-def create_ip_frame(ip, start_row=0, start_column=0):
+def create_ip_frame(ip, start_row, start_column, nas_status=False):
+    global selected_var
+    
     # Crée un cadre contenant l'adresse IP spécifiée
     ip_frame = Frame(
         ip_container,
-        width=250,
-        height=250,
+        width=200,
+        height=180,
         bg="#4B4949",
         bd=1
     )
     ip_frame.pack_propagate(0)  # Empêche le cadre de redimensionner son contenu
-    ip_frame.grid(row=start_row, column=start_column, padx=(10, 30), pady=20)
-    ip_label = Label(ip_frame, text=ip, bg="#4B4949", fg="white")
-    ip_label.pack(pady=80)
+    ip_frame.grid(row=start_row, column=start_column, pady=20, padx=10)
+
+    # Texte indiquant l'adresse IP
+    ip_label = Label(ip_frame, text=f"Serveur NAS : {ip}", bg="#4B4949", fg="white")
+    ip_label.pack(pady=10, anchor="center")
+
+    # Création d'une sous-frame pour le statut et la case à cocher
+    status_frame = Frame(ip_frame, bg="#4B4949")
+    status_frame.pack(pady=10)
+
+    # Point vert ou rouge indiquant l'état du NAS
+    status_color = "#089016" if nas_status else "#FF0000"
+    status_point = Label(status_frame, text="   ", bg=status_color)
+    status_point.pack(side=LEFT, padx=10)
+
+    # Texte indiquant l'état du NAS
+    status_label = Label(status_frame, text="En ligne" if nas_status else "Hors ligne", bg="#4B4949", fg="white")
+    status_label.pack(side=LEFT)
+
+    # Création de la case à cocher
+    check_var = BooleanVar()
+    switch = Checkbutton(ip_frame, text="Connecté", variable=check_var, bg="#4B4949", fg="white",
+                         selectcolor="#089016", activebackground="#089016", activeforeground="white")
+    switch.pack(anchor="center", pady=10)
+
+    # Ajouter un bouton de suppression à la sous-frame status_frame
+    delete_button = Button(ip_frame, text="Supprimer", command=lambda: delete_ip_frame(ip_frame), bg="#089016", relief="flat",
+    fg="white")
+    delete_button.pack(anchor="center", pady=10)
+
+    def on_check_changed():
+        global ip_address
+        if ip_frame.cget("bg") != "red":
+            if check_var.get():
+                ip_address = ip
+                print("Case cochée - IP mise à jour :", ip_address)
+                # Décocher tous les autres check boutons
+                for ip_frame_data in ip_frames:
+                    if ip_frame_data["ip_frame"] is not ip_frame:
+                        ip_frame_data["check_var"].set(False)
+            else:
+                ip_address = ""
+                print("Case décochée")
+
+    check_var.trace("w", lambda *args: on_check_changed())
+
+    # Ajouter les références aux éléments de la ip_frame dans une liste
+    ip_frame_data = {
+        "ip_frame": ip_frame,
+        "check_var": check_var
+    }
+    ip_frames.append(ip_frame_data)
+
+# Fonction pour supprimer une ip_frame
+def delete_ip_frame(ip_frame):
+    ip_frame.destroy()
+    # Supprimer l'ip_frame de la liste ip_frames
+    for ip_frame_data in ip_frames:
+        if ip_frame_data["ip_frame"] is ip_frame:
+            ip_frames.remove(ip_frame_data)
+            break
 
 def show_frame1():
     frame2.pack_forget()
@@ -109,7 +194,7 @@ def create_file_frame(file):
 
 # Création de la fenêtre
 window = Tk()
-window.title("HomeNAS")
+window.title("HomeNas")
 window.geometry("1000x600")
 window.configure(bg="#403c3c")
 
@@ -121,6 +206,8 @@ frame1.pack(anchor=NW)
 frame2 = Frame(window, bg="#403c3c",padx=200, pady=70)
 
 file_frames = []
+ip_frames = []
+
 
 # Création d'une zone de scroll pour la liste des fichiers
 scrollbar = Scrollbar(frame2)
@@ -128,6 +215,10 @@ scrollbar.pack(side=RIGHT, fill=Y)
 
 # Création d'un bouton pour mettre à jour la liste des fichiers
 update_button = Button(frame2, text="Mettre à jour", command=get_files_from_nas)
+
+# Création du bouton pour fermer le port
+close_button = Button(frame2, text="Fermer le port", command=close_port)
+close_button.pack()
 
 # Création d'un bouton b6
 b6 = Button(
@@ -160,9 +251,13 @@ scrollbar.pack(side=RIGHT, fill=Y)
 update_button = Button(frame2, text="Mettre à jour", command=get_files_from_nas)
 update_button.pack(pady=5)
 
+# Création du conteneur pour les boutons b0 et b1
+buttons_container = Frame(frame1, bg="#403c3c")
+buttons_container.grid(row=0, column=0, sticky="ew")
+
 # Création du conteneur pour les IP frames
-ip_container = Frame(window, bg="#403c3c")
-ip_container.place(x=200, y=280)
+ip_container = Frame(frame1, bg="#403c3c")
+ip_container.grid(row=1, column=0, sticky="n")
 
 rectangle = Canvas(
     window,
@@ -196,10 +291,9 @@ img3_path = os.path.join(file_dir, "Bouton_fichiers.png")
 img4_path = os.path.join(file_dir, "Bouton_parametres.png")
 img5_path = os.path.join(file_dir, "bouton_rafraichir.png")
 
-# Création des boutons
 img0 = PhotoImage(file=img0_path)
 b0 = Button(
-    frame1,
+    buttons_container,
     image=img0,
     borderwidth=0,
     highlightthickness=0,
@@ -210,7 +304,7 @@ b0.grid(row=0, column=0, padx=10)
 
 img1 = PhotoImage(file=img1_path)
 b1 = Button(
-    frame1,
+    buttons_container,
     image=img1,
     borderwidth=0,
     highlightthickness=0,
